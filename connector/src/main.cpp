d0028dfab1f216289e0c450a94848f5acf1ea634
Jezse <88537552+Jezse@users.noreply.github.com>
2022-12-19 11:11:45 +0200

    modified:   connector/platformio.ini 	modified:   connector/src/main.cpp 	modified:   python/coordinates.json 	deleted:    python/data/placement.csv 	modified:   python/main.py

--- connector/src/main.cpp
+++ connector/src/main.cpp
@@ -1,20 +1,32 @@
 #include <U8g2lib.h>
 #include <ESP8266WiFi.h>
+#include <Firebase_ESP_Client.h>
+
 #include <array>
 #include <String>
 #include <algorithm>
 #include <cmath>
 
-// Connector
-
+/*
+Connector
+ Test macros https://script.google.com/macros/s/AKfycbwvy-xTSn38CozpW-4b318F0Fltj2sCDKsZqAvHDZM3DS1MiCs0DFc94tKf8wlXpKSq/exec?Name="Vlad"&RSSI=-85
+ service ID AKfycbwvy-xTSn38CozpW-4b318F0Fltj2sCDKsZqAvHDZM3DS1MiCs0DFc94tKf8wlXpKSq
+ fingerprint 2B B5 F0 49 BB 7A DE CB 2F 32 67 2E 10 D5 45 3E BE 2C 18 0B
+*/
 #define NUMBER_OF_NETWORKS 3
-
+#define HTTP_PORT 8889
+#define FIREBASE_HOST "careful-form-371013-default-rtdb.europe-west1.firebasedatabase.app"
+#define FIREBASE_SECRET "MUFMetUYRPm1Dx09JbHSJD34vNNBuc8D3o6PgMI0"
 
 // U8g2 Contructor
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/16, /* clock=*/5, /* data=*/4);
 
 std::array<String, NUMBER_OF_NETWORKS> ssid = {"Test", "Vlad", "Test2"};
 std::array<String, NUMBER_OF_NETWORKS> pass = {"123456789", "0987654321a", "What is love"};
+
+FirebaseConfig config;
+FirebaseAuth auth;
+FirebaseData fbdo;
 
 void setup() {
   Serial.begin(9600);
@@ -25,6 +37,21 @@ void setup() {
   }
   u8g2.begin();
   u8g2.enableUTF8Print();
+  
+  config.database_url = FIREBASE_HOST;
+  config.signer.tokens.legacy_token = FIREBASE_SECRET;
+
+  Firebase.reconnectWiFi(true);
+
+  Firebase.begin(&config, &auth);
+}
+
+void send_data(String name, int value_rssi) {
+  if (Firebase.RTDB.setString(&fbdo, "/signal_" + name + "/SSID", name) && Firebase.RTDB.setInt(&fbdo, "/signal_" + name + "/RSSI", value_rssi)) {
+    Serial.printf("All ok \nSSID: %s\tRSSI: %i\n", name.c_str(), value_rssi); 
+  } else {
+    Serial.println("ERROR");
+  }
 }
 
 void loop() { 
@@ -36,29 +63,17 @@ void loop() {
 
   int number_of_stations = WiFi.scanNetworks();
   do {
-    u8g2.setCursor(0, 0);
-    int x = 0, y = 15;
-
     for (int i = 0; i < number_of_stations; ++i) {
       if (std::find(ssid.begin(), ssid.end(), WiFi.SSID(i)) != ssid.end()) {
-        u8g2.setCursor(x, y);
-        u8g2.print(WiFi.SSID(i));
-
-        x += 30;
 
         Serial.print(WiFi.SSID(i));
         Serial.printf(" %i", WiFi.RSSI(i));
         Serial.println();
-
-        u8g2.setCursor(x, y);
-        u8g2.print(WiFi.RSSI(i));
         
-        y += 15;
-        x = 0;
+        send_data(WiFi.SSID(i), WiFi.RSSI(i));
         delay(100);
       }
     }
     delay(1000);
   } while (u8g2.nextPage());
-  WiFi.begin();
 }
--- python/main.py
+++ python/main.py
@@ -1,14 +1,88 @@
 from sklearn.svm import SVC
 import numpy as np
-import threading
 import json
 
-RSSI_distant = []
+# JSON parameters
+# 1st anchor Test
+# 2nd anchor Test2
+# 3d anchor Vlad
 
-logs_RSSI = open("putty.txt", "r")
+distance_m = {
+		"Test": [],
+		"Test2": [],
+		"Vlad": []
+}
 
-with json.loads("coordinates.json") as coord:
-  for el in logs_RSSI:
-    pass
 
-logs_RSSI.close()
+coordinates = []
+
+
+def return_distance_m(d: dict, SSID: str, SSID_id: int, SSID_it: int) -> float:
+	return pow(10, ((d['RSSI'] - RSSI_coordinates[SSID_id][SSID][SSID_it])) / 20)
+
+
+def calc_matrix2(it_coord: int, x_y: str):
+	return 2 * (coordinates[it_coord][x_y] - coordinates[-2][x_y])
+
+
+with open("coordinates.json", 'r') as coord:
+	coordinates = json.load(coord)
+
+
+anchor_RSSI = coordinates[:-1:]
+RSSI_coordinates = coordinates[-1]
+names_anchor = []
+
+for el in RSSI_coordinates:
+		 names_anchor.append(list(el.keys())[-1])
+
+for anchor in range(len(anchor_RSSI)): # знаходження відстані від точки WiFi (або d по формулі з KW36 Localization Based on RSSI Ranging Application)
+		for i in range(len(RSSI_coordinates[anchor][names_anchor[anchor]])):
+				distance_m[names_anchor[anchor]].append(return_distance_m(anchor_RSSI[anchor], names_anchor[anchor], anchor, i))
+
+
+matrix1 = []
+matrix2 = []
+x_y = [{'name': el["name"],"x": el["x"],"y": el["y"]} for el in coordinates[:-1:]]
+
+def calc_matrix1(x, y, d, d_end) -> float:
+	return (x ** 2 + y ** 2 + d_end ** 2) - (x_y[-1]["x"] ** 2 + x_y[-1]["y"] ** 2 + d ** 2)
+
+
+def make_matrix2():
+  for it in range(len(coordinates[:-2:])):
+    temp_m = []
+    for i in ['x', 'y']:
+      temp_m.append(calc_matrix2(it, i))
+    matrix2.append(temp_m)
+
+
+make_matrix2()
+
+end_data = {
+	"Test": [],
+	"Test2": [],
+	"Vlad": []
+}
+
+for el in x_y[:-1:]:
+	for d in distance_m[el["name"]]:
+		temp = list()
+		temp.append(el["x"])
+		temp.append(el["y"])
+		temp.append(d)
+		end_data[el["name"]].append(temp)
+
+for el in distance_m["Vlad"]:
+	end_data["Vlad"].append(el)
+
+for it in range(len(end_data["Test"])):
+	temp = []
+	for name in names_anchor[:-1:]:
+		print(end_data[name][it][0], end_data[name][it][1], end_data[name][it][2], end_data[names_anchor[-1]][it])
+		temp.append(calc_matrix1(end_data[name][it][0], end_data[name][it][1], end_data[name][it][2], end_data[names_anchor[-1]][it]))
+	matrix1.append(temp)
+
+  
+
+	matrix1 = []
