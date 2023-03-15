0cfc7807917ede9f2307a0ee9f348067a1948563
Jezse <88537552+Jezse@users.noreply.github.com>
2023-03-15 22:43:10 +0200

    15/03/2023

--- Триангуляція/python/parser_from_firebase.py
+++ Триангуляція/python/parser_from_firebase.py
@@ -1,8 +1,7 @@
 import firebase_admin
 import json
-from time import sleep
 
-cred_obj = firebase_admin.credentials.Certificate('C:/What_needs_for_Vladick/MAN/Триангуляція/python/careful-form-371013-firebase-adminsdk-lijhc-bc6e9ac142.json')
+cred_obj = firebase_admin.credentials.Certificate('F:\\Projects\\MAN\\Триангуляція\\python\\careful-form-371013-firebase-adminsdk-lijhc-bc6e9ac142.json')
 
 default_app = firebase_admin.initialize_app(cred_obj, {
 	'databaseURL': "https://careful-form-371013-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -12,22 +11,18 @@ from firebase_admin import db
 
 names = ["Test_1", "Test_2", "Test_3"]
 RSSI_SIGNAL = []
+ref = db.reference("Value_Trio")
 
+with open("coordinates.json", "w") as file:
+    temp_1 = []
+    temp_2 = []
+    temp_3 = []
+    for i in range(1, len(ref.get()) + 1):
+        temp_1.append(ref.get()["Pair" + str(i)]["Test_1"])
+        temp_2.append(ref.get()["Pair" + str(i)]["Test_2"])
+        temp_3.append(ref.get()["Pair" + str(i)]["Test_3"])
+    RSSI_SIGNAL.append(temp_1)
+    RSSI_SIGNAL.append(temp_2)
+    RSSI_SIGNAL.append(temp_3)
 
-while True:
-    with open("coordinates.json", 'r') as file:
-        json_file = []
-        json_file = json.load(file)
-
-        for el in names:
-            RSSI_SIGNAL.append(db.reference(f"/signal_{el}"))
-
-        for i in range(3):
-            json_file[i].append(RSSI_SIGNAL[i].get()["RSSI"])
-
-        print(json_file)
-
-    with open("coordinates.json", "w") as file:
-        json.dump(json_file, file, indent=4)
-
-    sleep(30)
+    json.dump(RSSI_SIGNAL, file, indent=4)
